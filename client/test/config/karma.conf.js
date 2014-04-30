4dc0df52310b0e3d427f47449ff505e916734558
Martin Micunda <martinmicunda@hotmail.com>
2014-04-30 11:09:54 +0100

    Switch unit karma runner to PhantomJS

--- client/test/config/karma.conf.js
+++ client/test/config/karma.conf.js
@@ -92,7 +92,7 @@ module.exports = function (config) {
         // - PhantomJS
         // - IE (only Windows)
         // CLI --browsers Chrome,Firefox,Safari
-        browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
+        browsers: [process.env.TRAVIS ? 'Firefox' : 'PhantomJS'],
 //        browsers: ['Chrome', 'Safari', 'Firefox', 'PhantomJS'],
 
         // If browser does not capture in given timeout [ms], kill it

742dd3043ce411dd328ed1e97950784cbc2803fe
martinmicunda <martinmicunda@hotmail.com>
2014-05-18 19:02:26 +0100

    Fix bump task for travis

--- gulpfile.js
+++ gulpfile.js
@@ -557,7 +557,7 @@ gulp.task('check', 'Check if there are any changes to commit', function (cb) {
  * Publish 'build' folder to GitHub 'gh-pages' branch.
  */
 gulp.task('gh-pages', 'Publish \'build\' folder to GitHub \'gh-pages\' branch', function () {
-    console.log('no process' + process.env.TRAVIS);
+    console.log('no process' + GIT_REMOTE_URL);
     gulp.src(paths.build.basePath + '**/*')
         .pipe(ghPages(GIT_REMOTE_URL));
 });

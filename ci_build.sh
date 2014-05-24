55e70e51b9cd9494c6c70afc90300c7bc5989265
martinmicunda <martinmicunda@hotmail.com>
2014-05-24 01:13:22 +0100

    test

--- ci_build.sh
+++ ci_build.sh
@@ -57,7 +57,7 @@ function deploy_to_heroku {
     echo "   CheckHostIP no" >> ~/.ssh/config
     echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
     yes | heroku keys:add
-    yes | git subtree push --prefix build heroku master
+    yes | git subtree push --prefix build/dist/ heroku master
 #    git push heroku `git subtree split --prefix build/dist/ master`:master --force
 }
 

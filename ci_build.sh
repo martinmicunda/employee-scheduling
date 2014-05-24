0c373b8db843a6b3d5b23582c2789b7d47eca61a
martinmicunda <martinmicunda@hotmail.com>
2014-05-24 01:22:38 +0100

    test

--- ci_build.sh
+++ ci_build.sh
@@ -48,7 +48,7 @@ function clean_gh_pages_branch {
 function deploy_to_heroku {
     git rm -rf ./.gitignore
     git status
-    git add -f
+    git add .
     git commit -m "Add build folder for heroku"
     wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
     git remote add heroku git@heroku.com:employee-scheduling.git

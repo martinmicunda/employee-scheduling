36017e26e3f0b5ce936d74785cbc33387858769a
Travis-CI <travis@travis-ci.org>
2014-05-28 22:14:59 +0000

    Updated

---
+++ dist/server/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var config  = require('./config/config');
+
+/**
+ * Main application entry file.
+ * Note that the order of loading is important.
+ */
+
+// init the express application
+var app = require('./config/express')();
+
+// routes settings
+//require('./config/routes')(app);
+
+// Start up the server on the port specified in the config
+app.listen(process.env.PORT || config.get('express:port'), function () {
+    console.info(config.get('app:name') + ' app started on port: ' + (process.env.PORT || config.get('express:port')) + ' - with environment: ' + config.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;

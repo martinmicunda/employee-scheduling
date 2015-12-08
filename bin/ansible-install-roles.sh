03976a9fd28ad1f15e49aed61e1690f5a6e5fd78
martinmicunda <martinmicunda@hotmail.com>
2015-12-08 21:06:36 +0000

    chore(*): update dependencies

--- bin/ansible-install-roles.sh
+++ bin/ansible-install-roles.sh
@@ -2,13 +2,7 @@
 
 set -e
 ansible-galaxy install martinmicunda.common \
-                       martinmicunda.nodejs \
                        martinmicunda.iojs \
-                       martinmicunda.bower \
-                       martinmicunda.gulp \
-                       laggyluke.direnv \
-                       retr0h.nsq \
-                       savagegus.consul \
                        angstwad.docker_ubuntu \
                        franklinkim.docker-compose \
                        --force
--- bin/ansible-uninstall-roles.sh
+++ bin/ansible-uninstall-roles.sh
@@ -2,12 +2,6 @@
 
 set -e
 ansible-galaxy remove martinmicunda.common \
-                      martinmicunda.nodejs \
                       martinmicunda.iojs \
-                      martinmicunda.bower \
-                      martinmicunda.gulp \
-                      laggyluke.direnv \
-                      retr0h.nsq \
-                      savagegus.consul \
                       angstwad.docker_ubuntu \
                       franklinkim.docker-compose
